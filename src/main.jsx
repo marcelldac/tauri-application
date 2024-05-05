946eed525b022f318cc6c04c0fe9f87765f311dc
marcelldac <devmarcell@protonmail.com>
2024-05-04 23:00:06 -0300

    refactor: Update import statements and add routing functionality to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import "./styles.css"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import Home from './pages/Home'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
