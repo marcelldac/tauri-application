e6ff74908e4c3709ff1a0f00a58a9559bb9f5bf2
marcelldac <devmarcell@protonmail.com>
2024-05-05 20:23:18 -0300

    refactor: Update App component to include list of buildings

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { invoke } from '@tauri-apps/api';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+async function get_buildings () {
+  const response = await invoke('get_buildings')
+  return response
+}
+
 export default function App () {
+  const [buildings, setBuildings] = useState([])
+  
+  useEffect(() => {
+    (async () => {
+      const buildings = await get_buildings()
+      setBuildings(buildings)
+    })()
+  }, [])
+
   return (
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
       <h1>Dashboard Predios</h1>
       <Link to='login_manager'>Logar como Manager</Link>
+      <h2>Lista de prédios:</h2>
+      {buildings ? buildings.map((building) => {
+        return (
+          <div key={building.id}>
+            <span>{building.name}</span> | <span>{building.address}</span>
+          </div>
+        )
+      }) : <h1>Nenhum prédio foi encontrado</h1>}
     </div>
   )
 }
