442b1e78e0ab7049c93e0bc3cda6b6c0557012aa
marcelldac <devmarcell@protonmail.com>
2024-05-04 23:11:29 -0300

    refactor: Update login functionality and add get_users command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,6 +15,27 @@ fn login(email: &str, password: &str) -> (String, bool) {
 	}
 }
 
+async fn establish_connection() -> PgPool {
+	dotenv::dotenv().expect("Unable to load env variables from .env file");
+	let db_url: String = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+	PgPoolOptions::new()
+			.max_connections(5)
+			.connect(&db_url)
+			.await.expect("Unable to connect to postgres")
+}
+
+#[tauri::command]
+async fn get_users(state: State<'_, PgPoolWrapper>) -> Result<Vec<Record>, String> {
+    let rows: Vec<Record> = sqlx::query_as!(Record, r#"SELECT * FROM users"#)
+        .fetch_all(&state.pool)
+        .await
+        .expect("Unable to fetch users");
+
+    println!("{:?}", &rows);
+
+    Ok(rows)
+}
+
 fn main() {
 	tauri::Builder::default()
 		.invoke_handler(tauri::generate_handler![login])
