1da3726a4761489d4638a4536caa15a839065c56
marcelldac <devmarcell@protonmail.com>
2024-05-05 23:26:13 -0300

    refactor: Add logout functionality and user authentication in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 import { invoke } from '@tauri-apps/api';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { store } from './utils/store.js';
 
 async function get_buildings () {
   const response = await invoke('get_buildings')
   return response
 }
 
+function logout (setUserIsLogged) {
+  store.clear()
+  store.save()
+  setUserIsLogged(false)
+}
+
 export default function App () {
   const [buildings, setBuildings] = useState([])
+  const [userIsLogged, setUserIsLogged] = useState(false)
   
   useEffect(() => {
     (async () => {
       const buildings = await get_buildings()
+      const userLoggedId = await store.get('user-logged-id')
+      if (userLoggedId) setUserIsLogged(true)
       setBuildings(buildings)
     })()
-  }, [])
+  }, [userIsLogged])
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
       <h1>Dashboard Predios</h1>
-      <Link to='login_manager'>Logar como Manager</Link>
+      {
+      userIsLogged 
+      ? <button onClick={() => { logout(setUserIsLogged) }}>Sair</button> 
+      : <Link to='/login_manager'>Logar como Manager</Link>
+      }
       <h2>Lista de prédios:</h2>
       {buildings ? buildings.map((building) => {
         return (
