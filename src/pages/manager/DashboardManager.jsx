7d49b285d2131ef997d7cf595af43ce7333e2c99
marcelldac <devmarcell@protonmail.com>
2024-05-05 22:48:34 -0300

    refactor: Add logged user name to DashboardManager component

--- src/pages/manager/DashboardManager.jsx
+++ src/pages/manager/DashboardManager.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { store } from '../../utils/store';
 
 export default function DashboardManager () {
+  const [loggedUserName, setLoggedUserName] = useState()
+
+  useEffect(() => {
+    (async () => {
+      const { value: username } = await store.get('user-logged-username')
+      if(username){
+        setLoggedUserName(username)
+      }
+    })()
+  }, [])
+
   return(
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+      <p>Olá {loggedUserName}</p>
       <h1>Manager Dashboard</h1>
       <Link to='/create-building'>Criar um prédio</Link>
       <Link to='/'>Sair</Link>
